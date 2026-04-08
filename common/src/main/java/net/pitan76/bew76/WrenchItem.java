912402cca414551578a825a4be9086012d9c661f
PTOM76 <ptms76@outlook.jp>
2026-04-08 17:54:16 +0900

    update loader and dependency versions; refactor WrenchItem logic

--- common/src/main/java/net/pitan76/bew76/WrenchItem.java
+++ common/src/main/java/net/pitan76/bew76/WrenchItem.java
@@ -1,10 +1,5 @@
 package net.pitan76.bew76;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.pitan76.bew76.config.BEWConfig;
 import net.pitan76.mcpitanlib.api.entity.Player;
 import net.pitan76.mcpitanlib.api.event.item.ItemUseOnBlockEvent;
@@ -14,11 +9,13 @@ import net.pitan76.mcpitanlib.api.event.v0.InteractionEventRegistry;
 import net.pitan76.mcpitanlib.api.item.v2.CompatItem;
 import net.pitan76.mcpitanlib.api.item.v2.CompatibleItemSettings;
 import net.pitan76.mcpitanlib.api.util.*;
-import net.pitan76.mcpitanlib.api.util.block.BlockUtil;
 import net.pitan76.mcpitanlib.api.util.entity.ItemEntityUtil;
 import net.pitan76.mcpitanlib.api.util.math.BlockRotations;
-import net.pitan76.mcpitanlib.api.util.math.PosUtil;
+import net.pitan76.mcpitanlib.midohra.block.BlockState;
 import net.pitan76.mcpitanlib.midohra.item.ItemGroups;
+import net.pitan76.mcpitanlib.midohra.item.ItemStack;
+import net.pitan76.mcpitanlib.midohra.util.math.BlockPos;
+import net.pitan76.mcpitanlib.midohra.world.World;
 
 import static net.pitan76.bew76.BlockEntityWrench._id;
 
@@ -34,17 +31,18 @@ public class WrenchItem extends CompatItem {
             if (!BEWConfig.rotateFeature) return EventResult.pass();
 
             Player player = e.getPlayer();
-            ItemStack stack = e.getStackInHand();
+            ItemStack stack = ItemStack.of(e.getStackInHand());
 
             if (player.isSneaking()) return EventResult.pass();
-            if (!(stack.getItem() instanceof WrenchItem)) return EventResult.pass();
+            if (!(stack.instanceOf(WrenchItem.class))) return EventResult.pass();
             if (player.isClient()) return EventResult.success();
 
-            World world = e.getWorld();
-            BlockPos pos = e.getPos();
-            BlockState state = e.getBlockState();
+            World world = e.player.getMidohraWorld();
+            BlockPos pos = BlockPos.of(e.getPos());
+            BlockState state = BlockState.of(e.getBlockState());
 
-            WorldUtil.setBlockState(world, pos, BlockStateUtil.rotate(state, BlockRotations.CLOCKWISE_90));
+            world.setBlockState(pos, BlockState.of(
+                    BlockStateUtil.rotate(state.toMinecraft(), BlockRotations.CLOCKWISE_90)));
 
             return EventResult.success();
         });
@@ -57,25 +55,24 @@ public class WrenchItem extends CompatItem {
         if (e.isClient()) return e.success();
         if (!e.hasBlockEntity() || !e.player.isSneaking()) return e.pass();
 
-        if (BEWConfig.blacklistBlocks.contains(BlockUtil.toId(BlockStateUtil.getBlock(e.getBlockState())).toString()))
+        BlockState state = e.getMidohraState();
+
+        if (BEWConfig.blacklistBlocks.contains(state.getBlock().getId().toString()))
             return e.pass();
 
-        World world = e.getWorld();
-        BlockPos pos = e.getBlockPos();
-        BlockState state = e.getBlockState();
+        World world = e.getMidohraWorld();
+        BlockPos pos = e.getMidohraPos();
 
-        ItemStack dropStack = ItemStackUtil.create(state.getBlock());
+        ItemStack dropStack = state.getBlock().asItem().createStack();
 
         if (BEWConfig.saveBlockEntity) {
-            BlockEntityUtil.writeToStack(dropStack, e.getBlockEntity(), RegistryLookupUtil.getRegistryLookup(e.getBlockEntity()));
+            BlockEntityUtil.writeToStack(dropStack.toMinecraft(), e.getBlockEntity(), RegistryLookupUtil.getRegistryLookup(e.getBlockEntity()));
         }
 
-        ItemEntity itemEntity = ItemEntityUtil.create(world, PosUtil.x(pos) + 0.5, PosUtil.y(pos) + 0.5, PosUtil.z(pos) + 0.5, dropStack);
-        ItemEntityUtil.setToDefaultPickupDelay(itemEntity);
-        WorldUtil.spawnEntity(world, itemEntity);
+        ItemEntityUtil.createWithSpawn(world, dropStack, pos.toCenterVector3d());
 
-        WorldUtil.removeBlockEntity(world, pos);
-        WorldUtil.breakBlock(world, pos, false);
+        world.removeBlockEntity(pos);
+        world.breakBlock(pos, false);
 
         return e.success();
     }
